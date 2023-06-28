7262cb4749d85bdbad48549627ce9e98fd6706c2
Filipe <filipess671@gmail.com>
2023-06-28 04:52:20 -0300

    Feat: Funcao de cada operacao MIPS

--- src/models/pipeline/EX.java
+++ src/models/pipeline/EX.java
@@ -2,25 +2,57 @@ package models.pipeline;
 
 import models.abstracts.Instructions;
 import models.instruction.*;
+import models.utils.Utils;
 
 public class EX {
 
     public Instructions execute(Instructions instruction) {
         if (instruction instanceof Immediate) {
-            System.out.println(instruction.getOpcode());
-            System.out.println(instruction.getRs());
-            System.out.println(instruction.getRt());
-            System.out.println(instruction.getImmediate());
+            switch (instruction.getOpcode()) {
+                case "lw":
+                case "sw":
+                    instruction.setImmediate(instruction.getRs() + instruction.getOffset());
+                    break;
+                case "beq":
+                    if (instruction.getRs() == instruction.getRt())
+                        Utils.setPc(instruction.getOffset());
+                    break;
+                case "bne":
+                    if (instruction.getRs() != instruction.getRt())
+                        Utils.setPc(instruction.getOffset());
+                    break;
+                case "bltz":
+                    if (instruction.getRs() < instruction.getRt())
+                        Utils.setPc(instruction.getOffset());
+                    break;
+                case "bgtz":
+                    if (instruction.getRs() > instruction.getRt())
+                        Utils.setPc(instruction.getOffset());
+                    break;
+            }
+            return instruction;
         } else if (instruction instanceof Jump) {
-            System.out.println(instruction.getOpcode());
-            System.out.println(instruction.getTarget());
+            Utils.addToPc(instruction.getTarget());
+            return instruction;
         } else if (instruction instanceof Register) {
-            System.out.println(instruction.getOpcode());
-            System.out.println(instruction.getRs());
-            System.out.println(instruction.getRt());
-            System.out.println(instruction.getRd());
-            System.out.println(instruction.getShamt());
-            System.out.println(instruction.getFunct());
+            switch (instruction.getOpcode()) {
+                case "add":
+                    instruction.setResult(instruction.getRs() + instruction.getRt());
+                    break;
+                case "sub":
+                    instruction.setResult(instruction.getRs() + instruction.getRt());
+                    break;
+                case "mult":
+                    instruction.setResult(instruction.getRs() * instruction.getRt());
+                    break;
+                case "div":
+                    instruction.setResult(instruction.getRs() * instruction.getRt());
+                    instruction.setResult2(instruction.getRs() % instruction.getRt());
+                    break;
+                case "jr":
+                    Utils.setPc(instruction.getRs());
+                    break;
+            }
         } else if (instruction instanceof GetTC) {
             return instruction;
         }
